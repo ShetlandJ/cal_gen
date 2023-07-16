4c0d4ec526e24832401431cc63ad4f809d2c79ca
James <electionnightspecial@gmail.com>
2023-07-16 18:52:19 +0100

    get it working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="wrap">
+            <!-- text input -->
+            <label>
+                Event name
+                <input
+                    v-model="eventName"
+                    type="text"
+                    id="start"
+                    name="start"
+                />
+            </label>
+
+            <label>
+                Date
+                <input
+                    v-model="startDate"
+                    type="date"
+                    id="start"
+                    name="start"
+                    style="margin-bottom: 15px"
+                />
+            </label>
+
+            <a href="#" @click="generate">Generate</a>
+            <!-- <a href="#" onclick="javascript:cal.download('Back date')">Generate</a> -->
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { subWeeks } from "date-fns";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    data() {
+        return {
+            eventName: "",
+            startDate: new Date(),
+        };
+    },
+    methods: {
+        generate() {
+            // Generate the start and end dates dynamically using this.startDate
+            // const startDate = setMinutes(setHours(this.startDate, 8), 0);
+            // const endDate = setMinutes(setHours(this.startDate, 9), 0);
+
+            this.$ics.addEvent(
+                "en-us",
+                "James",
+                "",
+                "",
+                this.startDate,
+                this.startDate,
+                "",
+                {
+                    name: "Marilyn Stewart",
+                    email: "",
+                }
+            );
+
+            // Generate the second event's dates dynamically based on this.startDate
+            const startDate2 = subWeeks(new Date(this.startDate), 2);
+            const endDate2 = subWeeks(new Date(this.startDate), 2);
+
+            this.$ics.addEvent(
+                "en-us",
+                "James 2",
+                "",
+                "",
+                startDate2,
+                endDate2,
+                "",
+                {
+                    name: "Marilyn Stewart",
+                    email: "",
+                }
+            );
+
+            this.$ics.download("James");
+        },
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+.wrap {
+    margin: auto;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    height: 50px;
+    width: 100px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ICS from 'vue-ics'
+
+Vue.use(ICS, {
+  uidDomain: '',
+  prodId: '123'
+});
+
 Vue.config.productionTip = false
 
 new Vue({
